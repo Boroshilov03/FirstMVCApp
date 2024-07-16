d491944ae1ea18af291c3c876aacf6a15ff91f9b
Boroshilov03 <boroshilov.mirlan@gmail.com>
2024-07-15 22:57:50 -0700

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel() { Name="Ball", Age=13};
+            return View(doggo);
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
